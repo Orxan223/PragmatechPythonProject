5a6865f01cfcc0f647819e48248fc5ec6f958511
Orxan223 <or.xan.2018@bk.ru>
2021-06-09 13:32:50 +0400

    'update'

--- task/#day02.py
+++ task/#day02.py
@@ -1,11 +1,48 @@
 # 1)
+# a = list(int( input("Eded daxil edin :")))
+# a = list(int( input("Eded daxil edin :")))
+# a = list(int( input("Eded daxil edin :")))
+# a = list(int( input("Eded daxil edin :")))
+
+# if a[0] == a[1] == a[2] == a[3]:
+#     print(a[0] ** 2)
+# else:
+#     print(sum(a))
+
+# 2)
+l = list(input("eded daxil ele :"))
+l = list(input("eded daxil ele :"))
+l = list(input("eded daxil ele :"))
+l = list(input("eded daxil ele :"))
+
+
+a = l[0]
+for x in l:
+    x > a
+    a = x
+print("en boyuk eded : ", x)
+
+# 3)
+meyveler = {'alma' , 'armud' , 'banan'}
+a = input("meyve adi daxil edin :")
+if a in meyveler:
+    print(a)
+else:
+    print('error')
+
+
+
+
+
+
+# 4)
 a = [55, 42, 33, 27, 76,95]
 cem = 0
 for x in a:
     cem += x
 print(cem)
 
-# 2)
+# 5)
 list = [55, 99, 33, 27, 77]
 i = list[0]
 for x in list:
@@ -14,7 +51,7 @@ for x in list:
 
 print('En boyuk eded :', i)
 
-# 3)
+# 6)
 list = [55 , 42 , 13 , 27 , 1]
 i = list[0]
 for x in list:
@@ -23,26 +60,23 @@ for x in list:
 
 print('En kicik eded :', i)
 
-# 4)
-# list =  ['abc', 'xyz', 'aba', '1221','4554']
-# a = 0
-# for x in list:
-#     if len(x) >  x[0] == x[-1]: #2and
-#         a += 1
+# 7)
+list =  ['abc', 'xyz', 'aba', '1221','4554']
+a = 0
+for x in list:
+    if len(x) > 2 and  x[0] == x[-1]: 
+        a += 1
 
-# print(a)
+print(a)
 
 
-# 5)
+# 8)
 l = []
 if not l:
     print("List is empty")
 
-#6)
-# my_text = "Write a Python program to count the number of strings where the string length is 2 or more and the first and last character are same from a given list of strings"
-# a = my_text.split
 
-# 7)
+# 9)
 list = [55, 42, 33, 27, 78]
 i = []
 for x in list:

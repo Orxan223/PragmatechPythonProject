f81f339649c6df3d1821276f385073e60221bbb3
Orxan223 <or.xan.2018@bk.ru>
2021-06-10 03:29:37 +0400

    FINISH #DAY03 TASK

--- task/#da03.py
+++ task/#da03.py
@@ -1,3 +1,9 @@
+# ----------------------------SUAL----------------------------------------------------------------------------
+# Printde axirda 2 moterize verende error verir amma 3 edende duzelir.
+#  Meselen 11 , 20 line fikir versez 1 line artiqdir. 
+# Silindiyi zaman error verir. Sebebi nedir bunun?
+
+
 # 1)
 def sum(numbers):
     total = 0
@@ -38,3 +44,25 @@ def day(number):
 
 print(day(5))
 
+
+# 4)
+def LastElement(list):
+    for i in list:
+        if i == list[-1]:      
+            return i
+        else:
+            if i == []:
+                return "none"
+    
+print(LastElement((1,2,3,4,5,7)))
+
+
+# 5)
+def even(list):
+    a = []
+    for i in list:
+        if i % 2 == 0:
+            a.append(i)
+    return a
+
+print(even((1,2,3,4,5,6,7,8,9,10)))

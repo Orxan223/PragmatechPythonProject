b2fe614fc2b6a7013828c09786abb0fc66a98cc5
Orxan223 <or.xan.2018@bk.ru>
2021-06-20 21:20:25 +0400

    'update'

--- #week02/#WeekendTask.py
+++ #week02/#WeekendTask.py
@@ -2,34 +2,12 @@
 
 
 # ------------------------------------------------ HELE TAMAMLANMAYIB
-# import random
-
-# name = input('Birinci oyuncunun adi :')
-# nameX = input('Ikinci oyuncunun adi :')
-# print(f"Hormetli {name} ve {nameX} ")
-# print("Her birinizin 5 cehd etme shansiniz var.Her duzgun texmin size 1 xal qazandiracaq. Ugurlar")
-
-# number = random.randint(1,5)
-# guess = 0
-
-
-# while number != guess:
-#     guess = input("1 ve 5 arasinda reqem texmin edin :")
-#     guess = int(guess)
-
-#     if number == guess:
-#         print("Tebrikler")
-#         break
-
-#     else:
-#         print("Təəssüf , yeniden cehd edin ")
-
-
-
 import random
 
 
-number = random.randint(1, 10)
+number = random.randint(1, 5)
+print(number)
+list = [1, 2, 3, 4, 5]
 
 player_1 = []
 player_2 = []
@@ -45,45 +23,58 @@ player_1 = input('Birinci oyuncunun adi :')
 player_2 = input('Ikinci oyuncunun adi :')
 print(f"Hormetli {player_1} ve {player_2} ")
 print("Her birinizin 5 cehd etme shansiniz var.Her duzgun texmin size 1 xal qazandiracaq. Ugurlar!")
+x=1
+while(x<6):
 
-while number_of_guesses < 5:
-    guess = int(input())
-    guess = int(guess)
-    if guess != number:
-        print(f"{player_1} sefdir")
-    elif guess == number:
+
+    while (True):
+        guess_1 = int(input("player_1 Eded daxil edin :"))
+        guess_1 = int(guess_1)
+        if guess_1 != number_of_guesses:
+            print(f"{player_1} sefdir")
+        else:  
+            print("False")
+        break
+
+    if guess_1 == number_of_guesses:
         print(f'{player_1} tebrikler')
         player_1.append(xal)
-    else:
-        print("Teessuf")
-    break
+        print(f"player_1 sizin xaliniz{xal} ")
 
-while number_of_guesses < 5:
-    tex = int(input())
-    tex = int(guess)
-    if guess != number:
-        print(f"{player_2} sefdir")
-    elif guess == number:
+
+
+    while (True):
+        guess_2 = int(input("player_2 Eded daxil edin :"))
+        guess_2 = int(guess_2)
+
+        if guess_2 != number_of_guesses:
+            print(f"{player_2} sefdir")
+
+        else:
+            print("False")
+        break
+
+
+    if guess_2 == number_of_guesses:
         print(f'{player_2} tebrikler')
         player_2.append(xal)
-    else:
-        print("Teessuf")
-    break
+        print(f"player_2 sizin xaliniz{xal} ")
+            
 
 for x in player_1:
-    x += total_player_1
+    total_player_2 = total_player_1 + x
 print(f"{player_1} xaliniz :")
 
 
-for x in player_2:
-    x += total_player_2
+for y in player_2:
+    total_player_2 = total_player_2 + y
 print(f"{player_2} xaliniz :")
 
-if player_1 > player_2:
+if total_player_1 > total_player_2:
     print("Tebrikler siz qalibsiniz")
 
 
-if player_1 < player_2:
+if total_player_1 < total_player_2:
     print("Tebrikler siz qalibsiniz")
 
 

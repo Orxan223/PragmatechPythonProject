82818ea5f6dc4cd8c6216b7bfdae2fca956d974c
Orxan223 <or.xan.2018@bk.ru>
2021-06-23 05:39:45 +0400

    'taskCompleted'

--- #week03/Site/urls.py
+++ #week03/Site/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from look import views
+from firstapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),

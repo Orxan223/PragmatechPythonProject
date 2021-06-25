6258fbe5987606d5dd3cd743818ccbe32bccb94d
Orxan223 <or.xan.2018@bk.ru>
2021-06-26 00:35:22 +0400

    Week03FinishTask

--- #week03/firstapp/admin.py
+++ #week03/firstapp/admin.py
@@ -3,4 +3,27 @@ from .models import About
 # Register your models here.
 
 
-admin.site.register(About)
+
+
+class AboutAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    # Menasindan da gorunduyu kimi listdeki elemetleri display etmek ucundur
+
+    # fields = ('title',)
+    # Yalniz qeyd edilen hisseni gosderir
+
+    # exclude = ('title',)
+    # Qeyd edilen yer xaric , digerlerini gosderir
+
+    # list_filter = ('text',) 
+    # Admin sehifesinde sag hissede gorduyumuz filterleme isini gormek ucundur. Meselen
+    # qeyd etdiyimiz kimi text gore filterleme etsek ,veridiyimiz textlere esasen filterleme isleri aparilacaq
+
+    # search_fields = ('title',)
+    # Admin page-de serach ucun yer acilir, ve burada qeyd etdiyimiz title gore seacrh edib bize getirir
+admin.site.register(About,AboutAdmin)
+
+
+
+
+#DAY01-Settings aciqlamasi ucun ,acidigim Settigng_izahat.py baxin zehmet olmasa

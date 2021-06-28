fa8fbf167e0f4497190d1cc058454392240e9448
Orxan223 <or.xan.2018@bk.ru>
2021-06-29 03:47:37 +0400

    'update'

--- #week03/Site/settings.py
+++ #week03/Site/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'firstapp'
+    'firstapp',
+    'jet_django',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+JET_PROJECT = '{{your_project_name}}'
+JET_TOKEN = '{{your_jet_token}}'
--- #week03/Site/urls.py
+++ #week03/Site/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('firstapp.urls'))
-]
+    path('',include('firstapp.urls')),
+    path('jet_api/', include('jet_django.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+

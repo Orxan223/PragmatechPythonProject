a5167897d85db60a36559f537b63a6ca6435df4f
Orxan223 <or.xan.2018@bk.ru>
2021-07-13 14:57:52 +0400

    'update'

--- Project/app/templatetags/custom_tags.py
+++ Project/app/templatetags/custom_tags.py
@@ -1,8 +1,22 @@
+# from django import template
+# register = template.Library()
+    
+# from ..models import Skill
+  
+# @register.simple_tag
+# def any_function():
+#       return Skill.objects.count()
+
+# 2si eyni vaxda islemediyine gore commente aldim. Bunu isletmek isdedikde mes:<p>{{x.text}}{% any_function  %}</p> 
+# yazmagimiz kifayetdir.
+
+
 from django import template
 register = template.Library()
-    
-from ..models import Skill
-  
+
+import datetime
+
 @register.simple_tag
-def any_function():
-      return Skill.objects.count()
+def current_time(format_string):
+    return datetime.datetime.now().strftime(format_string)
+

ec7de6b326fef3d592813139b5b25f5043ff97fe
Orxan223 <or.xan.2018@bk.ru>
2021-07-17 10:14:31 +0400

    'update'

--- Project/Site/settings.py
+++ Project/Site/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-   
+
 ]
 
 MIDDLEWARE = [
@@ -80,12 +80,18 @@ WSGI_APPLICATION = 'Site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_name',
+        'USER' : 'username',
+        'PASSWORD' : '123',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432'
     }
 }
+
 
 
 # Password validation

395695deb9691b686a27723d0061c62d7f633924
Orxan223 <or.xan.2018@bk.ru>
2021-07-19 19:50:49 +0100

    update

--- Project/Site/settings.py
+++ Project/Site/settings.py
@@ -85,12 +85,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'db_name',
-        'USER' : 'username',
-        'PASSWORD' : '123',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432'
+        'USER':'user_name',
+        'PASSWORD':'123',
+        'HOST':'127.0.0.1',
+        'PORT': '5432',
     }
 }
+
+
 
 
 
